035de2ace908d8429bbe3c9b5823340954377782
Elissa Garber <elissagar@gmail.com>
2020-02-22 11:44:35 -0800

    Latest updates, started the context class

---
+++ UltimateShoppingList/ShoppingListContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UltimateShoppingList
+{
+	class ShoppingListContext : DbContext
+	{
+		public DbSet<Activity> ActivitiesTable { get; set; }
+		public DbSet<Groups> GroupsTable { get; set; }
+		public DbSet<Product> ProductsTable { get; set; }
+		public DbSet<ShoppingList> ShoppingListsTable { get; set; }
+		public DbSet<User> UsersTable { get; set; }
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ShoppingListDB;Integrated Security=True;Connect Timeout=30;");
+		}
+
+
+	}
+}
